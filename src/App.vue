c037615c59a1eb6cde0d38270d19238ac0f45cc0
Dusan Jankovic <44231983+Sayrax97@users.noreply.github.com>
2019-10-17 21:28:41 +0200

    update 17.10
    likes/dislikes
    links appear(not working)
    navbars
    audio

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <template>
   <div id="app">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg">
-            <b-form-input size="bg" class="mr-sm-2" placeholder="Search" v-model="search"></b-form-input>
-          </div>
-          <div>
-            <b-button size="bg"  @click="SearchWord">Search</b-button>
-          </div>
-        </div>
+    <b-navbar toggleable="lg" type="dark" variant="primary" class="fixed-top">
+      <b-navbar-brand>
+        <img src="../images/dictionary.png" alt="">
+        <span> UrbanDictionary</span>
+        </b-navbar-brand>
+      <b-navbar-nav class="ml-auto">
+          <b-form-input size="bg" class="mr-sm-2" placeholder="Search" v-model="search" @keyup.enter="SearchWord" ></b-form-input>
+          <b-button size="sm" class="my-2 my-sm-0 btn-success" @click="SearchWord">Search</b-button>
+      </b-navbar-nav>
+    </b-navbar>
+    <div class="container">
+      <div class="" v-if="podaci">
+        <post-container-vue  v-for="item in podaci" :key="item.defid" :post="item"></post-container-vue>
       </div>
-    <div v-if="podaci">
-    <post-container-vue  v-for="item in podaci" :key="item.defid" :post="item"></post-container-vue>
     </div>
+    <b-navbar toggleable="lg" type="dark" variant="secondary" :class="podaci? 'sticky-bottom':'fixed-bottom'">
+      <b-navbar-brand href="#">
+        <img src="../images/dictionary.png" alt="">
+        <span> UrbanDictionary</span>
+      </b-navbar-brand>
+      <b-nav-text>
+        Copyright &copy;2019 by WickeD
+      </b-nav-text>
+    </b-navbar>
   </div>
 </template>
 
@@ -27,7 +38,7 @@ export default {
   data: function() {
     return {
       podaci: "",
-      search:""
+      search:"",
     };
   },
   methods:{
@@ -55,8 +66,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
--- src/components/PostContainer.vue
+++ src/components/PostContainer.vue
@@ -1,31 +1,29 @@
 <template>
-  <div class="container ">
+  <div class="postStyle border border-secondary rounded container">
       <div class="row">
           <a class="font-weight-bold text-monospace h2" :href="post.permalink" target="_blank">{{post.word}}</a>
+          <button v-if="post.sound_urls" class=" rounded-circle btn" @click.prevent="playSound(post.sound_urls[0])">
+          <img src="../../images/play-button.png" alt="">
+          </button>
       </div>
-      <div class="row">
-          {{toLink(post.definition)}}
+      <div class="row definition font-weight-bolder " >
+        <p v-html="toLink(post.definition)">
+        </p>
       </div>
-      <div class="row">
-          {{toLink(post.example)}}
+      <div class="row example" >
+        <p v-html="toLink(post.example)">
+        </p>
       </div>
-      <div class="row">
+      <div class="row author">
           <div class="col-md-auto">
-            <b-button-group>
-                <b-button variant="success"><img src="../../images/like.png" alt="upvote"><span>{{post.thumbs_up}}</span></b-button>
-                <b-button variant="danger"><img src="../../images/dislike.png" alt="downvote"><span>{{post.thumbs_down}}</span></b-button>
+            <b-button-group class="ml-auto ">
+                <b-button variant="success" @click="upvote"><img src="../../images/like.png" alt="upvote"><span>{{post.thumbs_up}}</span></b-button>
+                <b-button variant="danger"  @click="downvote"><img src="../../images/dislike.png" alt="downvote"><span>{{post.thumbs_down}}</span></b-button>
             </b-button-group>
           </div>
           <div class="col-md">  
               <p class="text-center">by {{post.author}}--{{convertDate(post.written_on)}}</p>
           </div>
-      </div>
-      <div class="row">
-        <div class="col" v-for="audio in post.sound_urls" :key="audio">
-            <audio  controls>
-              <source :src="audio" type="audio/wav">
-            </audio>
-        </div>
       </div>
   </div>
 </template>
@@ -39,15 +37,15 @@ export default {
     toLink:function(d){
       var patt = /\[(.*?)\]/g;
       var result = d.match(patt);
-      var prefix = "<a href=''";
-      var mid = "'>";
+      var prefix = "<a href='";
+      var mid = "' onclick='clickedLink(this)'>";
       var sufix = "</a>";
-      var link="";
+      var link="#";
       var links=[];
       for(let i=0;i<result.length;i++)
       {
         result[i]=result[i].slice(1,result[i].length-1);
-        links.push(prefix+link+mid+result[i]+sufix);
+        links.push(" "+prefix+link+mid+result[i]+sufix+" ");
       }
       for(let i=0;i<links.length;i++)
       {
@@ -56,9 +54,25 @@ export default {
       }
       return d;
     },
+    clickedLink:function(link){
+      event.preventDefault
+      alert(link.innerHTML);
+    },
     convertDate(date){
         var dt=new Date(date);
         return dt.toDateString();
+    },
+    upvote(){
+      this.post.thumbs_up++;
+    },
+    downvote(){
+      this.post.thumbs_down++;
+    },
+    playSound (sound) {
+      if(sound) {
+        var audio = new Audio(sound);
+        audio.play();
+      }
     }
   }
 
@@ -66,5 +80,19 @@ export default {
 </script>
 
 <style scoped>
-
+.postStyle{
+  margin-top: 5px;
+  margin-bottom: 5px;
+}
+.definition{
+  margin-left: 10px;
+  padding: 5px;
+}
+.example{
+  margin-left: 10px;
+  padding: 5px;
+}
+.author{
+  margin: 5px;
+}
 </style>
